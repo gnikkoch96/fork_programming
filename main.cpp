af57c6270516b7873a116b46c5e6412318753f9f
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2020-03-20 13:00:41 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
     2. Child Process - Handles one Search process (Remember to deallocate)
 */
 
-void printArray(char arr[], int arrSize){
+void printArray(char *arr, int arrSize){
     for(int i = 0; i < arrSize; i++){
         cout << arr[i] << ", ";
     }
@@ -22,7 +22,7 @@ int main()
     int childPID = 10;      //Dummy Value
     int choice;             //(Parent) For text-based menu
     int arrSize;            //Determine the size of the array
-    int occurences;         //(Child) Count the # of times a letter has appeared
+    int occurences = 0;     //(Child) Count the # of times a letter has appeared
 
     char input;             //Can only be accessed in Parent Process
     char *arr;              //Can only be accessed in Child Process
@@ -42,6 +42,7 @@ int main()
 
 
         if(choice == 1){//Run Program
+            cout << "====Parent Process====" << endl;
             //Array Size
             cout << "Enter size: ";
             cin >> arrSize;
@@ -55,17 +56,19 @@ int main()
             childPID = fork();                      //Process is created where the child process knows the input and size of the array
             if(childPID == 0) //Child Process
             {
+                cout << "====Child Process====" << endl;
                 arr = new char[arrSize];
 
                 //Populates Array with Upper Case Letters (Randomly)
                 for(int i = 0 ; i < arrSize; i++){
-                    arr[i] = rand() % 26;
+                    arr[i] = alphabet[rand() % 26];
 
                     //Search and Count # of occurences of letter in the array
                     if(arr[i] == input)
                         ++occurences;
                 }
 
+                //(Debug) Check
                 printArray(arr, arrSize);
 
                 //Delete array after job is completed (De-allocate memory space to prevent memory leak)
